d9187340d20847be190e0c17cecefe6a42e55933
Viktor-Burakov <burakov.tmn@gmail.com>
2023-10-19 23:24:28 +0500

    feat(Request):
    add Request

---
+++ app/Http/Requests/Post/StoreRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests\Post;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'string',
+            'url' => 'string',
+            'date_start' => 'nullable|string',
+            'date_end' => 'nullable|string',
+            'preview_text' => 'nullable|string',
+            'H1' => 'string',
+            'preview' => 'nullable|string',
+            'preview_alt' => 'nullable|string',
+            'category' => '',
+        ];
+    }
+}
