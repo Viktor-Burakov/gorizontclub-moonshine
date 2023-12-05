f80066ceb0a77ee31440ef6928bf07d3c31164d9
Viktor-Burakov <burakov.tmn@gmail.com>
2023-12-05 15:32:36 +0500

    feat(Test):
    Add test DB

--- tests/Feature/PagesResponseTest.php
+++ tests/Feature/PagesResponseTest.php
@@ -5,12 +5,10 @@ namespace Tests\Feature;
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class PagesResponseTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
+
+    public function testAdminIndex(): void
     {
         $response = $this->get('/');
 
