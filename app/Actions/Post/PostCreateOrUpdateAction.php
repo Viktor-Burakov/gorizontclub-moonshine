8c4eaf45dfde30d9e16678599d87894dd20a0313
Viktor-Burakov <burakov.tmn@gmail.co>
2024-02-01 12:19:21 +0500

    refactor(postUpdate):
    add Class PostContentUpsert

--- app/Actions/Post/PostCreateOrUpdateAction.php
+++ app/Actions/Post/PostCreateOrUpdateAction.php
@@ -26,33 +26,8 @@ class PostCreateOrUpdateAction
         $contentBlocksSync = array();
         $ContentBlocksArrayIndex = array();
         if (isset($data['content_blocks'])) {
-            $contentBlocksToApsert = array();
-            $updatedAt = Carbon::now();
 
-            foreach ($data['content_blocks'] as $index => $contentBlock) {
-                if (!isset($contentBlock['id'])) {
-                    $contentBlock['id'] = null;
-                }
-
-                unset($contentBlock['images']);
-                $contentBlock['updated_at'] = $updatedAt;
-
-                $contentBlocksToApsert[] = $contentBlock;
-                $ContentBlocksArrayIndex[$contentBlock['name']] = $index;
-            }
-
-            ContentBlock::upsert(
-                $contentBlocksToApsert,
-                ['id'],
-                ['name', 'title', 'description',]
-            );
-            $contentBlocksArray = ContentBlock::select('id', 'name')
-                ->where('updated_at', $updatedAt)
-                ->get();
-
-            foreach ($contentBlocksArray as $cb) {
-                $contentBlocksSync[$cb->id] = ['sort' => $ContentBlocksArrayIndex[$cb->name]];
-            }
+            $contentBlocksSync = (new PostContentUpsert)($data['content_blocks'], new ContentBlock, ['name', 'title', 'description']);
 
             unset($data['content_blocks']);
         }
@@ -61,6 +36,7 @@ class PostCreateOrUpdateAction
         if (isset($data['images'])) {
             foreach ($data['images'] as $index => $image) {
                 if (!is_int($image['id'])) {
+                    dump($image['id']);
                     continue;
                 }
                 $images[$image['id']] = ['sort' => $index];
