1cc78dcba21d22d7828ab9a7e9e7dd9ba5a88ae2
Viktor Burakov <burakov.tmn@gmail.com>
2025-06-25 18:28:14 +0500

    feat(Seeder): Update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use App\Models\Category;
 use App\Models\ContentBlock;
 use App\Models\Image;
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -15,7 +16,7 @@ class UserSeeder extends Seeder
     {
         DB::table('moonshine_users')->insert([
             'name' => 'admin',
-            'password' => '$2y$10$JjkDbUtdJVwSFo9jqAL9XeTD2NGa3DtslOZEAolAfzQiFwfX6Z.TC',
+            'password' => Hash::make('admin'),
             'email' => 'admin',
         ]);
     }
